a13ccc4e7b9d105cacea3c269839c6f15e5bc54f
wallerina <wallerina01@gmail.com>
2026-08-07 00:47:31 +0300

    Сценарии нагрузки в Locust: User, TaskSet и SequentialTaskSet. Практика: Использование TaskSet для HTTP и gRPC API-клиентов

---
+++ grpc_locust_get_accounts.py
@@ -0,0 +1,68 @@
+from locust import task, User, between
+
+from clients.grpc.gateway.locust import GatewayGRPCTaskSet
+from contracts.services.gateway.accounts.rpc_get_accounts_pb2 import GetAccountsResponse
+from contracts.services.gateway.accounts.rpc_open_deposit_account_pb2 import (
+    OpenDepositAccountResponse,
+)
+from contracts.services.gateway.users.rpc_create_user_pb2 import CreateUserResponse
+
+
+class GetAccountsTaskSet(GatewayGRPCTaskSet):
+    """
+    Нагрузочный сценарий, который:
+    1. Создаёт нового пользователя.
+    2. Открывает депозитный счёт.
+    3. Запрашивает список всех счетов для текущего пользователя.
+
+    Использует базовый GatewayGRPCTaskSet и уже созданных в нём GRPC клиентов.
+    """
+
+    create_user_response: CreateUserResponse | None = None
+    open_deposit_account_response: OpenDepositAccountResponse | None = None
+    get_accounts_response: GetAccountsResponse | None = None
+    user_id: str
+
+    @task(2)
+    def create_user(self):
+        """
+        Создаём нового пользователя и сохраняем результат для последующих шагов.
+        """
+
+        self.create_user_response = self.users_gateway_client.create_user()
+
+    @task(2)
+    def open_deposit_account(self):
+        """
+        Открываем депозитный счёт для созданного пользователя.
+        Проверяем, что предыдущий шаг был успешным.
+        """
+
+        if not self.create_user_response:
+            return
+
+        self.user_id = self.create_user_response.user.id
+        self.open_deposit_account_response = (
+            self.accounts_gateway_client.open_deposit_account(user_id=self.user_id)
+        )
+
+    @task(6)
+    def get_accounts(self):
+        """
+        Получаем счета, если счёт был успешно открыт.
+        """
+
+        if not self.open_deposit_account_response:
+            return
+
+        self.accounts_gateway_client.get_accounts(user_id=self.user_id)
+
+
+class GetDocumentsUser(User):
+    """
+    Пользователь Locust, исполняющий сценарий получения счетов пользователя.
+    """
+
+    tasks = [GetAccountsTaskSet]
+    host = "localhost"
+    wait_time = between(1, 3)
