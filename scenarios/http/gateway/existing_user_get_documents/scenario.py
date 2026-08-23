a753c24d0f19c00377f40828cdd16ccbbbc101fe
wallerina <wallerina01@gmail.com>
2026-08-23 20:57:57 +0300

    Практика нагрузочного тестирования. Часть 3. Практика: Выпуск виртуальной карты существующим пользователем

---
+++ scenarios/http/gateway/existing_user_get_documents/scenario.py
@@ -0,0 +1,60 @@
+from locust import task, events
+from locust.env import Environment
+
+from clients.http.gateway.locust import GatewayHttpTaskSet
+from seeds.scenarios.existing_user_get_documents import (
+    ExistingUserGetDocumentsSeedsScenario,
+)
+from seeds.schema.result import SeedUserResult
+from tools.locust.user import LocustBaseUser
+
+
+# Этот хук выполняется один раз при инициализации теста (до старта пользователей).
+# Мы используем его, чтобы заранее прогнать сидинг и загрузить пользователей в память.
+@events.init.add_listener
+def init(environment: Environment, **kwargs):
+    # Создаем экземпляр сидинг-сценария
+    seeds_scenario = ExistingUserGetDocumentsSeedsScenario()
+
+    # Выполняем генерацию данных, если они ещё не созданы
+    seeds_scenario.build()
+
+    # Загружаем сгенерированных пользователей в окружение Locust
+    environment.seeds = seeds_scenario.load()
+
+
+# Набор задач (TaskSet), который будет выполняться виртуальными пользователями.
+class GetDocumentsTaskSet(GatewayHttpTaskSet):
+    # Типизируем объект пользователя из сидинга
+    seed_user: SeedUserResult
+
+    # Метод вызывается при запуске каждой сессии пользователя (до начала задач)
+    def on_start(self) -> None:
+        super().on_start()
+
+        # Получаем следующего пользователя из списка (по порядку!)
+        self.seed_user = self.user.environment.seeds.get_next_user()
+
+    @task(1)
+    def get_accounts(self):
+        # Запрашиваем список счетов
+        self.accounts_gateway_client.get_accounts(user_id=self.seed_user.user_id)
+
+    @task(2)
+    def get_tariff_document(self):
+        # Загружаем тарифный документ по сберегательному счёту
+        self.documents_gateway_client.get_tariff_document(
+            account_id=self.seed_user.savings_accounts[0].account_id
+        )
+
+    @task(2)
+    def get_contract_document(self):
+        # Загружаем договор по дебетовой карте
+        self.documents_gateway_client.get_contract_document(
+            account_id=self.seed_user.debit_card_accounts[0].account_id
+        )
+
+
+# Конкретный пользовательский класс, у которого в качестве задач используется GetDocumentsTaskSet
+class GetDocumentsScenarioUser(LocustBaseUser):
+    tasks = [GetDocumentsTaskSet]
---
+++ scenarios/http/gateway/existing_user_issue_virtual_card/scenario.py
@@ -0,0 +1,42 @@
+from locust import task, events
+from locust.env import Environment
+
+from clients.http.gateway.locust import GatewayHttpTaskSet
+from seeds.scenarios.existing_user_issue_virtual_card import (
+    ExistingUserIssueVirtualCardSeedsScenario,
+)
+from seeds.schema.result import SeedUserResult
+from tools.locust.user import LocustBaseUser
+
+
+@events.init.add_listener
+def init(environment: Environment, **kwargs):
+    seeds_scenario = ExistingUserIssueVirtualCardSeedsScenario()
+    seeds_scenario.build()
+    environment.seeds = seeds_scenario.load()
+
+
+class IssueVirtualCardTaskSet(GatewayHttpTaskSet):
+    seed_user: SeedUserResult
+    user_id: str
+    account_id: str
+
+    def on_start(self) -> None:
+        super().on_start()
+        self.seed_user = self.user.environment.seeds.get_random_user()
+        self.user_id = self.seed_user.user_id
+        self.account_id = self.seed_user.debit_card_accounts[0].account_id
+
+    @task(5)
+    def get_accounts(self):
+        self.accounts_gateway_client.get_accounts(user_id=self.user_id)
+
+    @task
+    def issue_virtual_card(self):
+        self.cards_gateway_client.issue_virtual_card(
+            user_id=self.user_id, account_id=self.account_id
+        )
+
+
+class IssueVirtualCardUser(LocustBaseUser):
+    tasks = [IssueVirtualCardTaskSet]
